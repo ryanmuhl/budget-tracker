4640b0b4ff20cfa30c14e9c0a1b9815745bf4198
ryanmuhl <ryanmuhl@hotmail.com>
2022-06-12 17:22:06 -0400

    update service-worker.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,3 +27,15 @@ request.onsuccess = function(event) {
     // log error here
     console.log(event.target.errorCode);
   };
+
+  // This function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access the object store for `new_pizza`
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+  
+    // add record to your store with add method
+    budgetObjectStore.add(record);
+  }
